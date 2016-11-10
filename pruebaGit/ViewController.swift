cc7e4855d3f26128f474f8c01f06dd2276fcd4b6
dborrajo <diego.borrajo@gmail.com>
2016-11-10 19:22:42 +0100

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Diego Borrajo Rodríguez on 10/11/16.
+//  Copyright © 2016 Diego Borrajo Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
